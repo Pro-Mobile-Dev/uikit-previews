6811ab0c0c127439d58d26475805555e3b12998a
Mark Kazakov <markazakov@gmail.com>
2025-05-02 15:48:48 +0300

    Initial Commit

---
+++ UIKitPreviews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPreviews
+//
+//  Created by Mark Kazakov on 02/05/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
